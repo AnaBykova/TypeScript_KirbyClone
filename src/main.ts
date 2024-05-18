871689407a1cf446ad8cd4669209fb0eb23902d7
AnaBykova <anabykovaca@gmail.com>
2024-05-18 18:10:56 -0400

    add player and camera setting to scene

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 //entry point of project
 
+import { makePlayer } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -46,6 +47,28 @@ async function gameSetup() {
 
     //draw map
     k.add(level1Layout);
+
+    //add kirb character
+    const kirb = makePlayer(
+      k,
+      //we have only one plpayer so x and y = 0
+      level1SpawnPoints.player[0].x,
+      level1SpawnPoints.player[0].y
+    );
+
+    //add kirb character to the scene
+    k.add(kirb);
+
+    //logic for camera, almost auto
+    k.camScale(0.7, 0.7);
+    //this onUpdate runs on every frame until object is destroyed, because not attached to any object
+    k.onUpdate(() => {
+      //make camera follow the player
+      if (kirb.pos.x < level1Layout.pos.x + 432)
+        //+500 = player on the left, lots of space on the right
+        k.camPos(kirb.pos.x + 500, 800);
+    });
+
   });
 
   //specify default scene
